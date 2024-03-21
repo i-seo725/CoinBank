15891a91078b9e1eb3eaf48f76ea019235464cf0
eunseo <hjing719@gmail.com>
2024-03-21 22:15:57 +0900

    Initial Commit

---
+++ CoinBank/CoinBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinBankApp.swift
+//  CoinBank
+//
+//  Created by 이은서 on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinBank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinBank
+//
+//  Created by 이은서 on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
