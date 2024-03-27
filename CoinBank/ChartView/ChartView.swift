a8f8503122e63df4b2c576d670c1983b223cd160
eunseo <hjing719@gmail.com>
2024-03-27 21:19:26 +0900

    차트뷰 ui 구현

--- CoinBank/ChartView/ChartView.swift
+++ CoinBank/ChartView/ChartView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct ChartView: View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        NavigationStack {
+            ScrollView {
+                GeometryReader { proxy in
+                    
+                    let graphWidth = proxy.size.width * 0.7 //차트 최대 너비
+                    
+                    VStack {
+                        ForEach(1..<10, id: \.self) { item in
+                            HStack {
+                                Text("테스트 중 - \(item)")
+                                    .font(.system(size: 14))
+                                    .frame(width: proxy.size.width * 0.2)
+                                ZStack(alignment: .leading) {
+                                    
+                                    Capsule()
+                                        .foregroundStyle(.lightPink)
+                                        .frame(maxWidth: .infinity)
+                                    Capsule()
+                                        .foregroundStyle(.coral)
+                                        .frame(width: CGFloat.random(in: 20...240), alignment: .leading)
+                                    
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding(.trailing, 4)
+                            }
+                            .frame(height: 36)
+                            .padding(.horizontal, 8)
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("실시간 호가")
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
